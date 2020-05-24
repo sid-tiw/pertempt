f2820287fca77474351ec5e4cda11bd992901a7e
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2020-05-24 13:55:30 +0530

    added the 'DFS like' algorithm to fetch the level-wise list

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -5,22 +5,16 @@
 #include <set>
 #include <curl/curl.h>
 #include <stdlib.h>
+#include <stack>
 #include "person.h"
 #include "operations.h"
 #ifdef WIN32
 #include <unistd.h>
 #endif
+#define MAX_LEVEL 2
 
-int main(int n_o_arg, char *arguments[])
+set<person> get_list(string user_name, CURL *pnt)
 {
-	if (n_o_arg <= 1)
-	{
-		cout << "No user name supplied! Aborting!!";
-		return 0;
-	}
-	string user_name = string(arguments[1]);
-	curl_global_init(CURL_GLOBAL_ALL);
-	CURL *pnt = curl_easy_init();
 	vector<person> pers1, pers2;
 	pers1 = fetch_list(user_name, _FIRST_, pnt);
 	pers2 = fetch_list(user_name, _SECOND_, pnt);
@@ -29,15 +23,53 @@ int main(int n_o_arg, char *arguments[])
 		pers.insert(pers1[i]);
 	for (int i = 0; i < pers2.size(); i++)
 		pers.insert(pers2[i]);
-	FILE *result = fopen("/home/sidtiw/Desktop/stlist.imd", "w");
-	for (auto it : pers)
+	return pers;
+}
+
+int main(int n_o_arg, char *arguments[])
+{
+	curl_global_init(CURL_GLOBAL_ALL);
+	CURL *pnt = curl_easy_init();
+	if (n_o_arg <= 2)
 	{
-		fprintf(result, "%s\n", it.get_name().c_str());
-		fprintf(result, "%s\n", it.get_uname().c_str());
-		fprintf(result, "%s\n", it.get_image().c_str());
-		fprintf(result, "%s\n", it.get_about().c_str());
-		fprintf(result, "%s\n", it.get_locu().c_str());
+		cout << "No user name supplied! Aborting!!";
+		return 0;
 	}
+	string user_name = string(arguments[1]);
+	string to_search = string(arguments[2]);
+	vector<pair<person, int>> master;
+	stack<pair<person, int>> driver, temp_driver;
+	set<person> level[MAX_LEVEL];
+	set<person> status;
+	level[0] = get_list(user_name, pnt);
+	for (auto &it : level[0])
+	{
+		driver.push(make_pair(it, 1));
+		master.push_back(make_pair(it, 1));
+	}
+	cout << "Done\n";
+	for (int i = 2; i <= MAX_LEVEL; i++)
+	{
+		while (!driver.empty()) //Yeah, BFS
+		{
+			if (status.find(driver.top().first) != status.end() || driver.top().first.get_uname() == user_name)
+			{
+				driver.pop();
+				continue;
+			}
+			level[i - 1] = get_list(driver.top().first.get_uname(), pnt);
+			for (auto &it : level[i - 1])
+			{
+				temp_driver.push(make_pair(it, i));
+				master.push_back(make_pair(it, i));
+			}
+			status.insert(driver.top().first);
+			driver.pop();
+		}
+		driver = temp_driver;
+	}
+	for (int i = 0; i < master.size(); i++)
+		master[i].first.print_details();
 	system("rm *.txt");
 	return 0;
 }
