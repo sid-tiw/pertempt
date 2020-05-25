ec877d2ebe37e62b8bb0a12cb9e45c80032b7cfe
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2020-05-26 03:59:43 +0530

    Added animations. (both CSS and JS)

--- JS/home.js
+++ JS/home.js
@@ -1,18 +1,36 @@
 let uname = document.getElementById("uname");
 let name = document.getElementById("name");
+let sbmt = document.getElementById("sbmt");
+
+sbmt.addEventListener('mouseenter', ()=>{
+	sbmt.className = "sbmtAnim";
+});
+
+sbmt.addEventListener('mouseleave', ()=>{
+	sbmt.className = "sbmt";
+});
 
 name.addEventListener('focusin', () => {
-	name.className = 'mid';
+	name.className = 'midn';
+	if(name.value == "search for")
+		name.value = "";
 });
 
 uname.addEventListener('focusin', () => {
-	uname.className = 'mid';
+	uname.className = 'midu';
+	if(uname.value == "your Username")
+		uname.value = "";
 });
 
 uname.addEventListener('focusout', () => {
-	uname.className = "inpt";
+	uname.className = "inpt up";
+	if(uname.value.length == 0)
+		uname.value = "your Username";
 });
 
 name.addEventListener('focusout', () => {
-	name.className = "inpt";
+	name.className = "inpt down";
+	if(name.value.length == 0)
+		name.value = "search for";
 });
+
