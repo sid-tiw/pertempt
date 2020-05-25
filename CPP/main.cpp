a5843b79607a300cad2dc371d44e9f958abdec14
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2020-05-25 17:08:45 +0530

    added an algorithm to rank search results

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <algorithm>
 #include <fstream>
 #include <errno.h>
 #include <set>
@@ -15,6 +16,9 @@
 #define _DELETE_ 0.2
 #define _REPLACE_ 0.1
 #define _ADD_ 0.2
+#define _FIRSTPR_ 30
+#define _SECONDPR_ 30
+#define calc_rank(first, second) first *_FIRSTPR_ + second *_SECONDPR_
 
 double find_difference(string first, string second)
 { //variable lavenshtein distance
@@ -47,12 +51,44 @@ double find_diff_index(pair<person, int> A, string search_string)
 	return ans;
 }
 
-vector<int, person> rank(vector<pair<person, int>> list, string to_search)
+vector<pair<person, int>> rank(vector<pair<person, int>> list, string to_search)
 {
+	sort(list.begin(), list.end());
+	vector<pair<pair<pair<double, double>, double>, person>> arr;
+	vector<pair<int, person>> temp1;
+	vector<pair<double, person>> temp2;
+	vector<pair<person, int>> arr1, arr2;
 	for (int i = 0; i < list.size(); i++)
 	{
-		
+		temp1.push_back(make_pair(list[i].second, list[i].first));
+		temp2.push_back(make_pair(find_diff_index(list[i], to_search), list[i].first));
 	}
+	sort(temp1.begin(), temp1.end());
+	sort(temp2.begin(), temp2.end());
+	int ranks1[temp1.size()], ranks2[temp2.size()];
+	ranks1[0] = 1;
+	ranks2[0] = 1;
+	for (int i = 1; i < temp1.size(); i++)
+	{
+		if (temp1[i].first == temp1[i - 1].first)
+			ranks1[i] = ranks1[i - 1];
+		else
+			ranks1[i] = ranks1[i - 1] + 1;
+		if (temp2[i].first == temp2[i - 1].first)
+			ranks2[i] = ranks2[i - 1];
+		else
+			ranks2[i] = ranks2[i - 1] + 1;
+		arr1.push_back(make_pair(temp1[i].second, (double)ranks1[i]));
+		arr2.push_back(make_pair(temp2[i].second, (double)ranks2[i]));
+	}
+	sort(arr1.begin(), arr1.end());
+	sort(arr2.begin(), arr2.end());
+	for (int i = 0; i < temp1.size(); i++)
+		arr.push_back(make_pair(make_pair(make_pair(calc_rank(arr1[i].second, arr2[i].second), list[i].second), temp2[i].first), arr1[i].first));
+	vector<pair<person, int>> to_return;
+	for (int i = 0; i < arr.size(); i++)
+		to_return.push_back(make_pair(arr[i].second, list[i].second));
+	return to_return;
 }
 
 set<person> get_list(string user_name, CURL *pnt)
