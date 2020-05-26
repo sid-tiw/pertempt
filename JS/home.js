90fa2c197f6720da0c2931134d2a6ee79adb364a
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2020-05-26 10:24:46 +0530

    fixed the scaling of the title

--- JS/home.js
+++ JS/home.js
@@ -1,36 +1,36 @@
-let uname = document.getElementById("uname");
-let name = document.getElementById("name");
-let sbmt = document.getElementById("sbmt");
+// let uname = document.getElementById("uname");
+// let name = document.getElementById("name");
+// let sbmt = document.getElementById("sbmt");
 
-sbmt.addEventListener('mouseenter', ()=>{
-	sbmt.className = "sbmtAnim";
-});
+// sbmt.addEventListener('mouseenter', ()=>{
+// 	sbmt.className = "sbmtAnim";
+// });
 
-sbmt.addEventListener('mouseleave', ()=>{
-	sbmt.className = "sbmt";
-});
+// sbmt.addEventListener('mouseleave', ()=>{
+// 	sbmt.className = "sbmt";
+// });
 
-name.addEventListener('focusin', () => {
-	name.className = 'midn';
-	if(name.value == "search for")
-		name.value = "";
-});
+// name.addEventListener('focusin', () => {
+// 	name.className = 'midn';
+// 	if(name.value == "search for")
+// 		name.value = "";
+// });
 
-uname.addEventListener('focusin', () => {
-	uname.className = 'midu';
-	if(uname.value == "your Username")
-		uname.value = "";
-});
+// uname.addEventListener('focusin', () => {
+// 	uname.className = 'midu';
+// 	if(uname.value == "your Username")
+// 		uname.value = "";
+// });
 
-uname.addEventListener('focusout', () => {
-	uname.className = "inpt up";
-	if(uname.value.length == 0)
-		uname.value = "your Username";
-});
+// uname.addEventListener('focusout', () => {
+// 	uname.className = "inpt up";
+// 	if(uname.value.length == 0)
+// 		uname.value = "your Username";
+// });
 
-name.addEventListener('focusout', () => {
-	name.className = "inpt down";
-	if(name.value.length == 0)
-		name.value = "search for";
-});
+// name.addEventListener('focusout', () => {
+// 	name.className = "inpt down";
+// 	if(name.value.length == 0)
+// 		name.value = "search for";
+// });
 
