96db7f583bda9ea8ddff3dd87b1561d7656e765f
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2020-05-27 16:57:32 +0530

    added node-static package to host pages

--- SERVER/main.js
+++ SERVER/main.js
@@ -1,36 +1,23 @@
-const http = require('http');
+var static = require('node-static');
+var http = require('http');
+var port = 1729;
+var hostname = '127.0.0.1';
+var file = new static.Server('../CLIENT/');
 
-const hname = "127.0.0.1";
-
-const port = 3000;
-
-const server = http.createServer((request, response) => {
-	let urlData = request.url;
-	let indUname = urlData.indexOf("uname=");
-	let indName = urlData.indexOf("&name=");
-	let file = "home.html";
-	console.log(indUname, indName);
-	response.statusCode = 200;
-	if (indUname != -1)
-		file = "info.html";
-	var flst = require('fs'),
-		path = require('path'),
-		filePath = path.join(__dirname, file);
-	flst.readFile(filePath, {
-		encoding: 'utf-8'
-	}, (err, data) => {
-		if (!err) {
-			response.writeHead(200, {
-				"content-Type": "text/html"
-			});
-			response.write(data);
-			response.end();
-		} else {
-			console.log(err);
-		}
-	});
+var server = http.createServer((request, response) => {
+	if (request.method == "POST") {
+		var body = "";
+        request.on('data', (chunk) => {
+            body += chunk;
+		});
+		console.log(body);
+	}
+	request.addListener('end', function () {
+		file.serve(request, response);
+	}).resume();
 });
 
-server.listen(port, hname, () => {
-	console.log("start");
+server.listen(port, hostname, () => {
+	var time = new Date();
+	console.log("server started at ", time.toUTCString());
 });
