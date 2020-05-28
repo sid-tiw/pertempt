02aa83fdf0550b316d60c04c5c55693e44b89245
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2020-05-28 22:34:55 +0530

    added comments

--- CPP/operations.cpp
+++ CPP/operations.cpp
@@ -5,6 +5,13 @@
 #include <fstream>
 #include "person.h"
 
+/**
+ * @brief parse out the list of github's persons from a github page of followers/following
+ * 
+ * @param ptr :::: a pointer to the file in which the fetched page is written. 
+ * @param srch :::: the string criteria defining the person in the page
+ * @return vector<person> :::: list of persons in form of vectors
+ */
 vector<person> parse(FILE *ptr, string srch)
 {
 	vector<int> positions;
@@ -29,6 +36,14 @@ vector<person> parse(FILE *ptr, string srch)
 		p_list.push_back(person(ptr, positions.at(i)));
 	return p_list;
 }
+/**
+ * @brief fetch the list of followers/following of a user
+ * 
+ * @param user_name :::: the user name of the user
+ * @param tab :::: either "followers" or "following"
+ * @param pnt :::: pointer returned by the global curl initializer
+ * @return pair<vector<person>, CURLcode> :::: a pair containing the list of persons and the status code
+ */
 
 pair<vector<person>, CURLcode> fetch_list(string user_name, string tab, CURL *pnt)
 {
